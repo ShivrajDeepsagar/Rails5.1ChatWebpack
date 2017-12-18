8f8c78499871117d446679fc4f5d9801501acbb6
Shivraj <itscool.sid@gmail.com>
2017-12-18 17:14:29 +0530

    finally rendering css through webpacker

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,13 @@ module EvilChat
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
