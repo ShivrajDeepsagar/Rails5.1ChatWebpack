9ea20809efc8d737ee7a925996d287a888571fdd
Shivraj <itscool.sid@gmail.com>
2017-12-31 12:49:15 +0530

    added helper method to make components name shorter while rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  module ApplicationHelper
+    def component(component_name, locals = {}, &block)
+      name = component_name.split("_").first
+      render("components/#{name}/#{component_name}", locals, &block)
+    end
+
+    alias :c :component
+  end
 end
